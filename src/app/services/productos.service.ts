9186e6484eecc22217126321a96ffe944db2d375
Winston Alonso <winston.alonglez@gmail.com>
2019-11-12 12:33:34 -0600

    Listo para la publicación

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Producto } from '../interfaces/producto.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
+  productos_idx: Producto[] = [];
+  cargando = true;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.cargarProductos();
+  }
+
+  private cargarProductos() {
+    return new Promise((resolve, reject) => {
+      this.http.get('https://angular-html-2cc31.firebaseio.com/productos_idx.json').subscribe(
+        (resp: Producto[]) => {
+          this.productos_idx = resp;
+          this.cargando = false;
+          resolve();
+        }
+      );
+    });
+  }
 }
